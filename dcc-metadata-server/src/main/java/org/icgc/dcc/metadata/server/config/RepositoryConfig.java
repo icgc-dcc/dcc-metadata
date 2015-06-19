41b3a87f41f7f4e0cb24c68af047119573a2d508
Bob Tiernay <btiernay@oicr.on.ca>
2015-06-19 10:48:24 -0400

    Simplified mongo configuration.

--- dcc-metadata-server/src/main/java/org/icgc/dcc/metadata/server/config/RepositoryConfig.java
+++ dcc-metadata-server/src/main/java/org/icgc/dcc/metadata/server/config/RepositoryConfig.java
@@ -18,16 +18,8 @@
 package org.icgc.dcc.metadata.server.config;
 
 import org.icgc.dcc.metadata.server.ServerMain;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 
 /**
  * Repository configuration.
@@ -36,25 +28,6 @@ import com.mongodb.MongoClientURI;
  */
 @Configuration
 @EnableMongoRepositories(basePackageClasses = ServerMain.class)
-public class RepositoryConfig extends AbstractMongoConfiguration {
-
-  @Autowired
-  RepositoryProperties properties;
-
-  @Override
-  protected String getDatabaseName() {
-    return properties.getDatabase();
-  }
-
-  @Bean
-  @Override
-  public Mongo mongo() throws Exception {
-    return new MongoClient(new MongoClientURI(properties.createUrl()));
-  }
-
-  @Override
-  public @Bean MongoTemplate mongoTemplate() throws Exception {
-    return new MongoTemplate(mongo(), getDatabaseName());
-  }
+public class RepositoryConfig {
 
 }
